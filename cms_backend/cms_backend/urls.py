1a0bea4ebf82e19353a4f082333433af5b3842e4
Arshad <arshaarshad21@gmail.com>
2024-01-22 14:29:05 +0530

    create api folder and inside urls.py file for all url intergarion

--- cms_backend/cms_backend/urls.py
+++ cms_backend/cms_backend/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
-    path('api/content/',include('content.urls')),
+    path('api/', include('api.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
